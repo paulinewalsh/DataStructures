4a2cbfd737a47e8ce63b6fca8b7a9ae3dcf557b7
Pauline Walsh <paulinecwalsh@gmail.com>
2017-10-29 11:01:45 -0400

    permutation

--- StringAlgorithms.java
+++ StringAlgorithms.java
@@ -1,7 +1,9 @@
 import java.util.Hashtable;
+import java.util.Arrays;
 
 public class StringAlgorithms {
 
+	// Complexity:  O(n) where n is string length
 	public static boolean allUnique(String s) {
 		Hashtable<String, Integer> chars = new Hashtable<String, Integer>();
 
@@ -11,13 +13,33 @@ public class StringAlgorithms {
 				return true;
 			}
 			else {
-
 				chars.put(c, 1);
 			}
 		}
 		return false;
 
 	}
+
+
+	// Complexity:  O(n log n) where n is string length
+	public static boolean isPermutation(String s1, String s2) {
+		if (s1.length() != s2.length()) {
+			return false;
+		}
+		char[] c1 = s1.toCharArray();  //O(n) 
+		char[] c2 = s2.toCharArray();  // O(n)
+
+		Arrays.sort(c1);  //O(n log n)
+		Arrays.sort(c2);  //O(n log n)
+
+		for (int i = 0; i < c1.length; i++) {  //O(n)
+			if (c1[i] != c2[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 
 
 	public static void main(String[] args) {
@@ -28,6 +50,16 @@ public class StringAlgorithms {
 		s = "abaabec";
 		unique = allUnique(s);
 		assert !unique;
+
+
+		String s1 = "abcdefg";
+		String s2 = "gfabedc";
+		boolean perm = isPermutation(s1, s2);
+		assert perm;
+
+		s2 = "hijklmno";
+		perm = isPermutation(s1, s2);
+		assert !perm;
 	}
 
 
