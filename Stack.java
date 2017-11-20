817d14614a23d51671de7ad6de2721c4178a8dc3
Pauline Walsh <paulinecwalsh@gmail.com>
2017-11-20 16:01:21 -0500

    prefer spaces to tabs

--- Stack.java
+++ Stack.java
@@ -4,43 +4,43 @@
  * @author Pauline Walsh
  * @version 1.0 2017.10.27
  */
-public class Stack<T> {
-	private LinkedList<T> stack;
+public class Stack<T extends Comparable> {
+    private LinkedList<T> stack;
 
-	public Stack() {
-		stack = new LinkedList<T>();
-	}
+    public Stack() {
+        stack = new LinkedList<T>();
+    }
 
-	public Stack(T data) {
-		stack = new LinkedList<T>(data);
-	}
+    public Stack(T data) {
+        stack = new LinkedList<T>(data);
+    }
 
-	public void push(T data) {
-		stack.insert(data);
-	}
+    public void push(T data) {
+        stack.insert(data);
+    }
 
-	public T pop() {
-		T data = this.peek();		
-		return data;
-	}
+    public T pop() {
+        T data = this.peek();       
+        return data;
+    }
 
-	public T peek() {
-		T data = stack.get(0);
-		return data;
-	}
+    public T peek() {
+        T data = stack.get(0);
+        return data;
+    }
 
 
-	public static void main(String[] args) {
-		Stack<Integer> stack = new Stack<Integer>(0);
-		for (int i = 0; i < 10; i++) {
-			stack.push(i);
-		}
-		int top = stack.peek();
-		assert top == 9;
-		top = stack.pop();
-		assert top == 9;
-		top = stack.pop();
-		assert top == 8;
-	}
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>(0);
+        for (int i = 0; i < 10; i++) {
+            stack.push(i);
+        }
+        int top = stack.peek();
+        assert top == 9;
+        top = stack.pop();
+        assert top == 9;
+        top = stack.pop();
+        assert top == 8;
+    }
 
 }
